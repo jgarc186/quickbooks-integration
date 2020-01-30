b746d8f2c8afeb1c56924f69f734d67c88219d10
Jose Garcia <josegarcia@Joses-MBP-2.attlocal.net>
2020-01-30 12:21:15 -0500

    Working on the quickbooks service provider

--- app/Services/QuickbooksService.php
+++ app/Services/QuickbooksService.php
@@ -9,9 +9,13 @@ use QuickBooks_WebConnector_Server;
 class QuickBooksService extends QuickBooks_Utilities
 {
     protected $queue;
-    protected $log_level = QUICKBOOKS_LOG_DEBUG;
-    protected $soapserver = QUICKBOOKS_SOAPSERVER_BUILTIN;
-    protected $soap_options = [
+    protected $dsn = "";
+    protected $errMap = [];
+    protected $handlerOptions = [];
+    protected $driverOptions = [];
+    protected $callbackOptions = [];
+
+    protected $soapOptions = [
         // See http://www.php.net/soap
     ];
 
@@ -29,8 +33,16 @@ class QuickBooksService extends QuickBooks_Utilities
         // QuickBooks_WebConnector_Handlers::HOOK_LOGINSUCCESS => '_quickbooks_hook_loginsuccess', 	// Run this function whenever a successful login occurs
     ];
 
+    /**
+     * QuickBooksService constructor.
+     * @param string $dsn
+     * @param string $user
+     * @param string $pass
+     */
     public function __construct(string $dsn, string $user, string $pass)
     {
+        $this->dsn = $dsn;
+
         if (! static::initialized($dsn)) {
             static::initialize($dsn);
             static::createUser($dsn, $user, $pass);
@@ -40,41 +52,21 @@ class QuickBooksService extends QuickBooks_Utilities
 
     /**
      * Create a new QuickBooks SOAP server
-     *
-     * @param mixed $dsn        		Either a DSN-style connection string *or* a database resource (if reusing an existing connection)
-     * @param array $map				An associative array mapping queued commands to function/method calls
-     * @param array $errmap			    An associative array mapping error codes to function/method calls
-     * @param array $hooks				An associative array mapping events to hook function/method calls
-     * @param string $logLevel			The path to the WSDL file to use for the SOAP server methods
-     * @param array $soap_options		Options to pass to the SOAP server (these mirror the default PHP SOAP server options)
-     * @param array $handler_options	Options to pass to the handler class
-     * @param array $driver_options		Options to pass to the driver class (i.e.: MySQL, etc.)
      */
-    public function sendRequest(
-        string $dsn,
-        array $map,
-        array $errmap,
-        array $hooks,
-        string $logLevel = QUICKBOOKS_LOG_NORMAL,
-        $soap = QUICKBOOKS_SOAPSERVER_PHP,
-        $wsdl = QUICKBOOKS_WSDL,
-        $soap_options = array(),
-        $handler_options = array(),
-        $driver_options = array(),
-        $callback_options = array()
-    ) {
+    public function sendRequest()
+    {
         $Server = new QuickBooks_WebConnector_Server(
-            $dsn,
-            $map,
-            $errmap,
-            $hooks,
-            $logLevel,
-            $soapserver,
+            $this->dsn,
+            $this->map,
+            $this->errMap,
+            $this->hooks,
+            QUICKBOOKS_LOG_NORMAL,
+            QUICKBOOKS_SOAPSERVER_PHP,
             QUICKBOOKS_WSDL,
-            $soap_options,
-            $handler_options,
-            $driver_options,
-            $callback_options
+            $this->soapOptions,
+            $this->handlerOptions,
+            $this->driverOptions,
+            $this->callbackOptions
         );
 
         return $Server->handle(true, true);
